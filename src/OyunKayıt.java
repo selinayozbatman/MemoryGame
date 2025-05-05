95673723161053053c9cc54bffbb6af25da9b38c
Selinay Özbatman <selinaysirmaci@gmail.com>
2025-05-05 17:00:45 +0300

    "initial commit"

---
+++ src/OyunKayıt.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class OyunKayıt {
+
+    public static void oyunKaydet(Kart[][] kartlar ){
+        try(ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream("kayit.bin"))){
+            System.out.println("oyun kaydediliyor...");
+
+            out.writeObject(kartlar);
+
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    public static Kart[][] kayittanAl(){
+        try(ObjectInputStream in = new ObjectInputStream(new FileInputStream("kayit.bin"))){
+
+            Kart[][] cikti = (Kart[][]) in.readObject();
+            return cikti;
+
+        } catch (IOException ex) {
+            Logger.getLogger(OyunKayıt.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(OyunKayıt.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return null;
+    }
+}
